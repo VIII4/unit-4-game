8f4d9475842cd00b7eccae5efa3723bed484d4b4
Lay84 <Iyan.laylor@gmail.com>
2020-02-17 12:24:54 -0800

    created character portraits. modified character object contruct to include creating html character card through jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,66 +17,104 @@ var playerObj;
 var targetObj;
 
 var characterContainer = [];
-var enemyContainer =[];
+var enemyContainer = [];
 
 //Character Object Base
 
-function CharacterObject(_hp, _atkPwr, _cntrPwr, ){
-	
-	////Variables
-	
-	
-	//Character Attributes
-	this.atkPwr = _atkPwr;
-	this.cntrPwr = _cntrPwr;
-	this.hp = _hp;
-	this.currentHp =0;
-	this.currentAtkPwr = 0;
-	
-	//HTML Elements
-	this.hpDisplay;
-	this.popUpInfo;
-	
-	////Methods
-	
-	//Set up character
-	this.setUp = function(){
-		this.currentHp = this.hp
-		this.currentAtkPwr = this.atkPwr
-	}
-	
-	//increase Attack Power
-	this.increaseAtkPwr = function(){
-		this.currentAtkPwr +-= this.atkPwr;
-	}
-	
-	//Recieve Damage
-	this.rcvDamage = function(_atk){
-		this.currentHp -= _atk;
-	}
-	
-	
-	//Combat
-	this.combat = function(_trgtChar){ 
-		_trgtChar.rcvDamage(atkPwr)
-		this.hp -= _trgtChar.cntrPwr;
-		this.increaseAtkPwr();
-	}
-	
-	//Reset
-	this.reset = function(){
-		this.currentHp = this.hp
-		this.currentAtkPwr = this.atkPwr
-    }
-    
-    //Update display
-    this.updateDisplay = function()
-    {
-        // this.hpDisplay.html()
-	    // this.popUpInfo;
-    }
+function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
+  ////Variables
 
-	
+  //Character Attributes
+  this.name = _name;
+  this.atkPwr = _atkPwr;
+  this.cntrPwr = _cntrPwr;
+  this.hp = _hp;
+  this.currentHp = 0;
+  this.currentAtkPwr = 0;
+  this.imgPath = _imgPath;
+
+  //HTML Elements
+  this.characterPanel;
+  this.hpDisplay;
+  this.popUpInfo;
+
+  ////Methods
+
+  //Set up character
+  this.setUp = function() {
+    this.currentHp = this.hp;
+    this.currentAtkPwr = this.atkPwr;
+  };
+
+  //increase Attack Power
+  this.increaseAtkPwr = function() {
+    this.currentAtkPwr += this.atkPwr;
+  };
+
+  //Recieve Damage
+  this.rcvDamage = function(_atk) {
+    this.currentHp -= _atk;
+  };
+
+  //Combat
+  this.combat = function(_trgtChar) {
+    _trgtChar.rcvDamage(atkPwr);
+    this.hp -= _trgtChar.cntrPwr;
+    this.increaseAtkPwr();
+  };
+
+  //Reset
+  this.reset = function() {
+    this.currentHp = this.hp;
+    this.currentAtkPwr = this.atkPwr;
+  };
+
+  //Update display
+  this.updateDisplay = function() {
+    // this.hpDisplay;
+    // this.popUpInfo;
+  };
+
+  this.createPanel = function() {
+    var other = this.name;
+    var path = this.imgPath;
+
+    this.characterPanel = $("<div></div>");
+    this.characterPanel.attr("id", other);
+    this.characterPanel.attr("class", "col");
+    this.characterPanel.append(
+      "<div class = 'card border-0 bg-transparent'> <img src= " +
+        path +
+        " class = 'card-img'/> <div class='card-img-overlay'>  <div class='row > <div class='col'> <p id= " +
+        other +
+        "'nameDisplay'></p></div>// </div>  <div class='row > <div class='col'> <p id= " +
+        other +
+        "'health'></p></div> //  </div>  </div> </div>"
+    );
+  };
 }
 
+//Create all Unique Character Objects
+var darthMaulObj = new CharacterObject(
+  "darthMaul",
+  120,
+  20,
+  15,
+  "../assets/images/darthmaul.png"
+);
 
+var debug = function() {
+  console.log(darthMaulObj.atkPwr, darthMaulObj.name, darthMaulObj.hp);
+};
+
+var setGame = function() {
+  //Create all character cards and append to selection panel
+  //For each character object in character container, create a element in the panel
+  darthMaulObj.createPanel();
+  $("#charSelectionPanel").append(darthMaulObj.characterPanel);
+};
+
+$(document).ready(function() {
+  setGame();
+  /////////
+});
