cf6457a4e89ff11b79dd79d48e947532c4f5fc9e
Lay84 <Iyan.laylor@gmail.com>
2020-02-17 21:30:57 -0800

    updated game script to functioning combat with states, win and loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,8 @@ var attackButton;
 
 //Global Variables
 
+var gameState = 0; //0 = Setup,  1 = Player Selection, 2= Enemy Selection, 3 = Combat
+
 var winCounter;
 var lossCounter;
 
@@ -18,6 +20,9 @@ var targetObj;
 
 var characterContainer = [];
 var enemyContainer = [];
+
+//Instructions
+var gameInstructions;
 
 //Character Object Base
 
@@ -32,11 +37,15 @@ function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
   this.currentHp = 0;
   this.currentAtkPwr = 0;
   this.imgPath = _imgPath;
+  this.eliminated = false;
 
   //HTML Elements
   this.characterPanel;
   this.hpDisplay;
   this.popUpInfo;
+
+  //Test
+  this.idString;
 
   ////Methods
 
@@ -44,6 +53,10 @@ function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
   this.setUp = function() {
     this.currentHp = this.hp;
     this.currentAtkPwr = this.atkPwr;
+
+    //recreate ID tag for health display and store ref
+    var temp = "#" + this.name + "health";
+    this.hpDisplay = $(temp);
   };
 
   //increase Attack Power
@@ -58,8 +71,9 @@ function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
 
   //Combat
   this.combat = function(_trgtChar) {
-    _trgtChar.rcvDamage(atkPwr);
-    this.hp -= _trgtChar.cntrPwr;
+    var dmg = this.currentAtkPwr;
+    _trgtChar.rcvDamage(dmg);
+    this.currentHp -= _trgtChar.cntrPwr * 2;
     this.increaseAtkPwr();
   };
 
@@ -71,8 +85,7 @@ function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
 
   //Update display
   this.updateDisplay = function() {
-    // this.hpDisplay;
-    // this.popUpInfo;
+    this.hpDisplay.text(this.currentHp);
   };
 
   this.createPanel = function() {
@@ -81,40 +94,193 @@ function CharacterObject(_name, _hp, _atkPwr, _cntrPwr, _imgPath) {
 
     this.characterPanel = $("<div></div>");
     this.characterPanel.attr("id", other);
-    this.characterPanel.attr("class", "col");
+    this.characterPanel.attr("class", "col-2 characterCard");
     this.characterPanel.append(
-      "<div class = 'card border-0 bg-transparent'> <img src= " +
+      "<div class = 'card border-0 bg-transparent'> <img class= 'portrait' src= " +
         path +
-        " class = 'card-img'/> <div class='card-img-overlay'>  <div class='row > <div class='col'> <p id= " +
+        " class = 'card-img'/> <div class='card-img-overlay'>  <div class='row' > <div class='col'> <p class= ' text-center portraitText' id=" +
         other +
-        "'nameDisplay'></p></div>// </div>  <div class='row > <div class='col'> <p id= " +
+        "nameDisplay>" +
         other +
-        "'health'></p></div> //  </div>  </div> </div>"
+        "</p></div></div>  <div class='row '> <div class='col'> <p class= 'portraitText text-center' id='" +
+        other +
+        "health'>" +
+        this.hp +
+        " </p></div></div>  </div> </div>"
     );
   };
 }
 
 //Create all Unique Character Objects
 var darthMaulObj = new CharacterObject(
-  "darthMaul",
+  "Darth_Maul",
   120,
   20,
   15,
-  "../assets/images/darthmaul.png"
+  "../unit-4-game/assets/images/darthmaul.png"
 );
 
-var debug = function() {
-  console.log(darthMaulObj.atkPwr, darthMaulObj.name, darthMaulObj.hp);
-};
+var lukeSkywalkerObj = new CharacterObject(
+  "Luke_Skywalker",
+  190,
+  30,
+  10,
+  "../unit-4-game/assets/images/lukekywalker.png"
+);
+
+var darthVaderObj = new CharacterObject(
+  "Darth_Vader",
+  150,
+  25,
+  25,
+  "../unit-4-game/assets/images/darthvader.png"
+);
+
+var yodaObj = new CharacterObject(
+  "Yoda",
+  135,
+  40,
+  10,
+  "../unit-4-game/assets/images/yoda.png"
+);
+
+//Store Characters in container
+var characterContainer = [
+  darthMaulObj,
+  darthVaderObj,
+  lukeSkywalkerObj,
+  yodaObj
+];
 
 var setGame = function() {
   //Create all character cards and append to selection panel
   //For each character object in character container, create a element in the panel
-  darthMaulObj.createPanel();
-  $("#charSelectionPanel").append(darthMaulObj.characterPanel);
+
+  if (gameState === 0) {
+    for (i = 0; i < characterContainer.length; i++) {
+      characterContainer[i].createPanel();
+    }
+  }
+
+  for (i = 0; i < characterContainer.length; i++) {
+    $("#charSelectionPanel").append(characterContainer[i].characterPanel);
+  }
+
+  //Set Game State to Player Selection
+  gameState = 1;
+};
+
+//Method that happens when user clicks on any character image
+var clickAction = function(_state, _obj) {
+  //Player Selection.
+  if (_state === 1) {
+    //Assign Obj as Player
+    playerObj = _obj;
+    playerObj.setUp();
+    playerObj.eliminated = true;
+    console.log("Player chooses " + playerObj.name + " as thier Character");
+
+    //Move card to Player panel
+    $("#selectedPlayerCard").append(_obj.characterPanel);
+
+    //Move other Cards to Enemies Panel
+    for (i = 0; i < characterContainer.length; i++) {
+      if (characterContainer[i].name != _obj.name) {
+        $("#enemySelectionPanel").append(characterContainer[i].characterPanel);
+      }
+    }
+
+    //Change State to 2
+    gameState = 2;
+  } else if (_state === 2) {
+    //check if already assigned as player...
+    if (_obj.name === playerObj.name) {
+      alert(
+        playerObj.name +
+          " already Selected as Player, Select another character as your Enemy"
+      );
+      return;
+    }
+    //Character not assigned
+    else {
+      //Assign Obj as Target
+      targetObj = _obj;
+      targetObj.setUp();
+      console.log("Player chooses " + targetObj.name + " as thier Enemy");
+
+      //Move Card to Target Panel
+      $("#targetPanel").append(_obj.characterPanel);
+
+      //Change state to 3
+      gameState = 3;
+    }
+  } else {
+    console.log("State  0 or 3");
+    return;
+  }
+};
+
+var combatAction = function() {
+  //Check if in combat State
+  if (gameState === 3) {
+    console.log("In Combat ");
+    console.log(
+      playerObj.name +
+        "with Health of " +
+        playerObj.currentHp +
+        " Vs " +
+        targetObj.name +
+        "with Health of " +
+        targetObj.currentHp
+    );
+    //Player attacks target, deals damage and recieves counter damage.
+    playerObj.combat(targetObj);
+    playerObj.updateDisplay();
+    targetObj.updateDisplay();
+    //update display of HP
+
+    //check HP of player and Target if player is zero game over, if enemy is choose new enemy
+    if (targetObj.currentHp <= 0) {
+      alert("Enemy Eliminated choose new target");
+      targetObj.eliminated = true;
+      gameState = 2;
+      //Add way to check if all enemies eliminated
+    } else if (playerObj.currentHp <= 0) {
+      alert("Player Eliminated, Game Over");
+      gameState = 1;
+      setGame();
+    }
+  }
+  //If not in combat state return
+  else {
+    console.log("Not in Combat");
+    return;
+  }
+};
+
+var debug = function() {
+  ////////
 };
 
 $(document).ready(function() {
   setGame();
-  /////////
+
+  //Test
+
+  //Character Buttons
+  darthVaderObj.characterPanel.on("click", function() {
+    clickAction(gameState, darthVaderObj);
+  });
+  darthMaulObj.characterPanel.on("click", function() {
+    clickAction(gameState, darthMaulObj);
+  });
+  yodaObj.characterPanel.on("click", function() {
+    clickAction(gameState, yodaObj);
+  });
+  lukeSkywalkerObj.characterPanel.on("click", function() {
+    clickAction(gameState, lukeSkywalkerObj);
+  });
+
+  //Attack Button
+  $("#attackButton").on("click", combatAction);
 });
